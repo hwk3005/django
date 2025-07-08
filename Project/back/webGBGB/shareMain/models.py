a07f978b0f75a4319fa3907fe00a146f8b124bac
hwk3005 <hwk3005@naver.com>
2025-07-08 22:03:23 +0900

    P_Back_250708

--- Project/back/webGBGB/shareMain/models.py
+++ Project/back/webGBGB/shareMain/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 class ReadingGroup(models.Model):
-    name = models.CharField(max_length=100)
-    member_count = models.IntegerField()
+    group_name = models.CharField(max_length=100)
+    member_count = models.IntegerField(default=2)
     description = models.TextField(blank=True)
+    book = models.CharField(max_length=50)
+    tag = models.CharField(max_length=50)
+    is_public = models.IntegerField(default=0)
+    password = models.CharField(max_length=50)
+    
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.name}, {self.member_count}, {self.description}'
+        return f'{self.group_name}, {self.member_count}, {self.description}'
--- Project/back/webGBGB/shareMain/views.py
+++ Project/back/webGBGB/shareMain/views.py
@@ -4,21 +4,36 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 # 교환독서_그룹만들기 | Share_AddGroup
-@csrf_exempt  # 임시로 CSRF 막기 (실제 배포할 땐 csrf_token 꼭 쓰기!)
+# @csrf_exempt  # 임시로 CSRF 막기 (실제 배포할 땐 csrf_token 꼭 쓰기!)
 def Share_AddGroup(request):
-    if request.method == 'POST':
-        name = request.POST.get('group_name	')
-        member_count = request.POST.get('member_count')
-        desc = request.POST.get('description')
-
+    if request.method == 'GET':
+        return render(request,'shareMain/Share_AddGroup.html')
+    elif request.method == 'POST' :
+        # 회원정보 DB저장
+        group_name = request.POST.get('group_name')
+        member_count = int(request.POST.get('member_count',2))
+        description = request.POST.get('description')
+        book = request.POST.get('book')
+        tag = request.POST.get('tag')
+        is_public = int(request.POST.get('is_public',0))
+        password = request.POST.get('password')
+    
         ReadingGroup.objects.create(
-            name=name,
+            group_name=group_name,
             member_count=member_count,
-            description=desc
+            description=description,
+            book=book,
+            tag=tag,
+            is_public=is_public,
+            password=password,
         )
-        return redirect('/Share_Main/')  # 생성 후 목록 페이지로 이동
-    return render(request, 'shareMain/Share_AddGroup.html')  # GET 요청 시 폼 띄우기
-
+        print("그룹 저장 완료")
+        
+        # 방법 1: render로 context 전달
+        groups = ReadingGroup.objects.all()
+        
+        context = {'groups': groups,'tag':tag,'group_name':group_name,'book':book }
+        return render(request, 'shareMain/Share_Main.html', context)
 
 
 
